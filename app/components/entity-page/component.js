05f90798a48e5e77bf523958fab8d4a8a7fe3ae9
loun4 <iheb02@yahoo.fr>
2017-09-17 18:06:14 +0200

    Entity page component

---
+++ app/components/entity-page/component.js
@@ -0,0 +1,13 @@
+
+import Ember from 'ember';
+
+const {
+  Component
+} = Ember;
+
+
+export default Component.extend({
+  data: [],
+  sections: [],
+  relatedEntities: []
+});
--- tests/integration/components/entity-page/component-test.js
+++ tests/integration/components/entity-page/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('person-page', 'Integration | Component | person page', {
+moduleForComponent('entity-page', 'Integration | Component | entity page', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{person-page}}`);
+  this.render(hbs`{{entity-page}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#person-page}}
+    {{#entity-page}}
       template block text
-    {{/person-page}}
+    {{/entity-page}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
