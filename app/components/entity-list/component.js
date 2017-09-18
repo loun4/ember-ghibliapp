f7c0895fbb973badb4664fb4c06f32e306fe2c0a
loun4 <iheb02@yahoo.fr>
2017-09-18 14:45:51 +0200

    Implement entity-list sort

--- app/components/entity-list/component.js
+++ app/components/entity-list/component.js
@@ -1,15 +1,37 @@
 
 import Ember from 'ember';
+import { comparator } from '../../utils';
 
 const {
   Component,
   computed
 } = Ember;
 
+
 export default Component.extend({
   tagName: 'table',
   classNames: 'ui sortable selectable celled table black entity-list',
   data: [],
   cols: [],
-  link: null
+  link: null,
+  sortBy: null,
+  sortDirection: 'descending',
+
+  sortedData: computed('data', 'cols', 'sortBy', 'sortDirection', function() {
+    const data = this.get('data');
+    const by = this.get('sortBy');
+    const { type } = this.get('cols').find(({key}) => key == by);
+    const direction = this.get('sortDirection');
+
+    return data.sort(comparator(by, type, direction));
+  }),
+
+  actions: {
+    handleSort(by) {
+      this.setProperties({
+        sortBy: by,
+        sortDirection: this.get('sortDirection') === 'descending' ? 'ascending': 'descending'
+      });
+    }
+  }
 });
--- app/utils.js
+++ app/utils.js
@@ -1,3 +1,27 @@
 
 export const filterByEntityId = (entity, entityId, item) =>
     item.get(entity).includes(entityId);
+
+
+export const comparator = (by, type, direction) => {
+  const directions = {
+    ascending : 1,
+    descending: -1
+  };
+
+  direction = directions[direction];
+
+  const fns = {
+    number  : (a, b) => (
+      direction * (a.get(by) - b.get(by))
+    ),
+    string  : (a, b) => (
+      direction * String(a.get(by)).localeCompare(b.get(by))
+    ),
+    date  : (a, b) => (
+      direction * (new Date(a.get(by)) - new Date(b.get(by)))
+    )
+  }
+
+  return fns[type];
+};
